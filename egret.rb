69d947b67f318d452559e799a152741549d1282f
Arpith Siromoney <arpith@feedreader.co>
2018-02-12 11:22:32 -0500

    http 1.1 😊

--- egret.rb
+++ egret.rb
@@ -6,5 +6,5 @@ host = ARGV[0]
 addrinfo = Socket.getaddrinfo(host, "http")[0]
 s = Socket.new Object.const_get(addrinfo[0]), SOCK_STREAM
 s.connect Socket.sockaddr_in(addrinfo[1], addrinfo[2])
-s.write( "GET / HTTP/1.0\r\n\r\n" )
+s.write( "GET / HTTP/1.1\r\nHost: #{host}\r\nConnection: close\r\n\r\n" )
 p s.read
