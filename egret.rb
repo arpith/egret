99aafdf512ce8cb7342afbd33033c1f4abc10d24
Arpith Siromoney <arpith@feedreader.co>
2018-02-12 11:48:45 -0500

    support requests for a specific path 🤓

--- egret.rb
+++ egret.rb
@@ -1,10 +1,11 @@
 require 'socket'
 require 'ipaddr'
+require 'uri'
 include Socket::Constants
 
-host = ARGV[0]
-addrinfo = Socket.getaddrinfo(host, "http")[0]
+uri = URI(ARGV[0])
+addrinfo = Socket.getaddrinfo(uri.host, uri.scheme)[0]
 s = Socket.new Object.const_get(addrinfo[0]), SOCK_STREAM
 s.connect Socket.sockaddr_in(addrinfo[1], addrinfo[2])
-s.write( "GET / HTTP/1.1\r\nHost: #{host}\r\nConnection: close\r\n\r\n" )
+s.write( "GET #{uri.path} HTTP/1.1\r\nHost: #{uri.host}\r\nConnection: close\r\n\r\n" )
 p s.read
